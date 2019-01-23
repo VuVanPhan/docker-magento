6d62eef671d3a1912fdce7a286988799dc6535df
Phan <kaioshin@trueplus.vn>
2019-01-23 17:54:43 +0700

    Complete script install magento 2 with docker tool

--- README.md
+++ README.md
@@ -12,10 +12,10 @@ Or echo "127.0.0.1 m2.io | tee -a /etc/hosts"
 ```sh
 1 - Open the terminal on the Nginx directory
 2 - Go to the directory "Nginx...-PHP...-Mariadb10.1.26/magento/"
-# Example : $ cd Nginx1.13-PHP71-Mariadb10.1.26/magento/
+# Example : $ cd Nginx1.13-PHP7.1-Mariadb10.1.26/magento/
 3 - Create folder "source" and up code Magento to here
 4 - Return to the directory contain the docker-composer.yml - the directory "Nginx...-PHP...-Mariadb10.1.26"
-# Example : Here, is the directory "Nginx1.13-PHP71-Mariadb10.1.26"
+# Example : Here, is the directory "Nginx1.13-PHP7.1-Mariadb10.1.26"
 # $ cd ../
 5 - Run "docker-compose up"
 
@@ -23,8 +23,8 @@ Or echo "127.0.0.1 m2.io | tee -a /etc/hosts"
 # - Nginx...-PHP...-Mariadb10.1.26/docker-composer.yml
 # - Nginx...-PHP...-Mariadb10.1.26/nginx/default.php.conf
 # Example :
-# - Nginx1.13-PHP71-Mariadb10.1.26/docker-composer.yml
-# - Nginx1.13-PHP71-Mariadb10.1.26/nginx/default.php.conf
+# - Nginx1.13-PHP7.1-Mariadb10.1.26/docker-composer.yml
+# - Nginx1.13-PHP7.1-Mariadb10.1.26/nginx/default.php.conf
 ```
 
 ```sh
@@ -45,7 +45,7 @@ Or echo "127.0.0.1 m2.io | tee -a /etc/hosts"
 # Access to SSH to run
 # $ docker exec -u www-data -it {container_name} bash
 # $ docker exec -u www-data -it {container_id} bash
-$ docker exec -u www-data -it nginx113-php71-mariadb10126_fpm1371_1 bash
+$ docker exec -u www-data -it nginx113-php71-mariadb10126_fpm11371_1 bash
 $ cd source
 # $ ../install_magento DATABASE_NAME URL
 $ ../install_magento nginx113php71_magentoce226 http://m2.io:8710/
@@ -67,5 +67,5 @@ $ ../install_magento nginx113php71_magentoce226 http://m2.io:8710/
 ```sh
 # $ docker exec -u www-data -it {container_name} bash
 # $ docker exec -u www-data -it {container_id} bash
-$ docker exec -u www-data -it nginx113-php71-mariadb10126_fpm1371_1 /bash
+$ docker exec -u www-data -it nginx113-php71-mariadb10126_fpm11371_1 /bash
 ```
---
+++ deployenvmagento.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+#echo "Welcome to the magento 2 automation environment building program with docker tool"
+## define build environment magento 2
+#echo -n "Enter Nginx version : "
+#read nginxversion
+#
+#echo -n "Enter PHP version : "
+#read phpversion
+#
+#echo -n "Enter MariaDB version : "
+#read mariadbversion
+
+nginxversion=1.14
+phpversion=7.1
+mariadbversion=10.1.26
+
+
+instance="Nginx"${nginxversion}"-PHP"${phpversion}"-Mariadb"${mariadbversion}
+
+# source environment
+source ./$instance/env
+
+# create source magento 2
+rm -rf $PWD"/"$instance"/magento/source" "data"
+mkdir -p $PWD"/"$instance"/magento/source"
+cd $PWD"/"$instance"/magento/source"
+chmod -R 777 ./
+wget https://github.com/magento/magento2/archive/2.2.6.tar.gz
+wget https://github.com/magento/magento2-sample-data/archive/2.2.6.tar.gz
+tar -xzvf 2.2.6.tar.gz
+tar -xzvf 2.2.6.tar.gz.1
+mv -v magento2-2.2.6/* ./
+mv -v magento2-2.2.6/.* ./
+cp -avr magento2-sample-data-2.2.6/* ./
+rm -rf magento2-2.2.6/ magento2-sample-data-2.2.6/ 2.2.6.tar.gz 2.2.6.tar.gz.1
+
+# Change or modify the directory permission to fit Apache2 configuration.
+find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
+find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
+chown -R ubuntu:www-data .
+chmod u+x bin/magento
+
+# Install PHP Composer
+apt-get install -y composer
+
+# Install Magento components using the PHP composer. Ignore platform request when not compatible with PHP
+composer install --ignore-platform-reqs
+
+# docker compose
+docker-compose up -d
+
+# install magento with
+containerphpfpm=$(echo "${instance//.}" | tr '[:upper:]' '[:lower:]')"_fpm"$(echo "${nginxversion//.}" | tr '[:upper:]' '[:lower:]')$(echo "${phpversion//.}" | tr '[:upper:]' '[:lower:]')"_1"
+containermysql=$(echo "${instance//.}" | tr '[:upper:]' '[:lower:]')"_db_1"
+
+# create databases
+docker exec -u www-data -it $containermysql sh
+mysql -u root -p
+
+create database magento;
+create user magento identified by 'magento123@#';
+grant all privileges on magento.* to magento@localhost identified by 'magento123@#';
+flush privileges;
+exit;
+
+# install magento site with command line
+docker exec -u www-data -it $containerphpfpm bash
+cd source/
+../install_magento magento http://192.168.120.75:8710/
